6df8f3bab73abad024aadf61e1baafc517c7d0e2
Chandler <escalachandler@gmail.com>
2024-03-03 20:08:19 +0800

    Added Index Controller

---
+++ Escala_CA-CRUD/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Escala_CA_CRUD.Repository;
+
+namespace Escala_CA_CRUD.Controllers
+{
+    public class BaseController : Controller
+    {
+        public dbsys32Entities _db;
+        public BaseRepository<user> _userRepo;
+        public BaseController()
+        {
+            _db = new dbsys32Entities();
+            _userRepo = new BaseRepository<user>();
+        }
+    }
+}
---
+++ Escala_CA-CRUD/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Escala_CA_CRUD.Controllers
+{
+    public class HomeController : BaseController
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<user> userList = _userRepo.GetAll();
+            return View(userList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(user u)
+        {
+            _userRepo.Create(u);
+            TempData["Msg"] = $"User {u.username} added!";
+            return RedirectToAction("Index");
+        }
+    }
+}
