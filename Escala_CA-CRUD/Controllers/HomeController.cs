152c210a00c213c54d9c16977adca8a11a7a1bc4
Chandler <escalachandler@gmail.com>
2024-03-03 20:23:38 +0800

    Updated Home Controller
    Added Create, Edit, Delete, Details

--- Escala_CA-CRUD/Controllers/HomeController.cs
+++ Escala_CA-CRUD/Controllers/HomeController.cs
@@ -26,5 +26,27 @@ namespace Escala_CA_CRUD.Controllers
             TempData["Msg"] = $"User {u.username} added!";
             return RedirectToAction("Index");
         }
+        public ActionResult Details(int id)
+        {
+            return View(_userRepo.Get(id));
+        }
+        public ActionResult Edit(int id)
+        {
+            return View(_userRepo.Get(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(user u)
+        {
+            _userRepo.Update(u.id, u);
+            TempData["Msg"] = $"User {u.username} updated!";
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            _userRepo.Delete(id);
+            TempData["Msg"] = $"User deleted!";
+            return RedirectToAction("Index");
+        }
     }
 }
